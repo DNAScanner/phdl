9cac449427db6a8970ca5b1fa79d2b65a21c5d3c
DNAScanner <contact@dnascanner.de>
2026-02-09 20:19:03 +0100

    Added re-export for types

--- mod.ts
+++ mod.ts
@@ -1,5 +1,7 @@
 import { type Account, type AccountVideo, Order, type StreamData, type Video } from "./types.ts";
 
+export type * from "./types.ts"
+
 const headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
 };
